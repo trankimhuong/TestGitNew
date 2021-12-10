c16f803b65f34f386815c9b29b4325eb3f685be7
trankimhuong <tkhuongfb@gmail.com>
2021-12-10 09:53:33 +0700

    push linq to EF

---
+++ DemoLINQToEF/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace DemoLINQToEF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvNV = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.txtHoNV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLuong = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMaCV = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaBP = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvNV
+            // 
+            this.dgvNV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNV.Location = new System.Drawing.Point(25, 151);
+            this.dgvNV.Name = "dgvNV";
+            this.dgvNV.Size = new System.Drawing.Size(593, 213);
+            this.dgvNV.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã NV";
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(130, 19);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(142, 20);
+            this.txtMaNV.TabIndex = 2;
+            // 
+            // txtHoNV
+            // 
+            this.txtHoNV.Location = new System.Drawing.Point(130, 61);
+            this.txtHoNV.Name = "txtHoNV";
+            this.txtHoNV.Size = new System.Drawing.Size(142, 20);
+            this.txtHoNV.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Họ NV";
+            // 
+            // txtTenNV
+            // 
+            this.txtTenNV.Location = new System.Drawing.Point(130, 103);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.Size = new System.Drawing.Size(142, 20);
+            this.txtTenNV.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tên NV";
+            // 
+            // txtLuong
+            // 
+            this.txtLuong.Location = new System.Drawing.Point(423, 22);
+            this.txtLuong.Name = "txtLuong";
+            this.txtLuong.Size = new System.Drawing.Size(142, 20);
+            this.txtLuong.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Lương";
+            // 
+            // txtMaCV
+            // 
+            this.txtMaCV.Location = new System.Drawing.Point(423, 64);
+            this.txtMaCV.Name = "txtMaCV";
+            this.txtMaCV.Size = new System.Drawing.Size(142, 20);
+            this.txtMaCV.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(346, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Mã CV";
+            // 
+            // txtMaBP
+            // 
+            this.txtMaBP.Location = new System.Drawing.Point(423, 106);
+            this.txtMaBP.Name = "txtMaBP";
+            this.txtMaBP.Size = new System.Drawing.Size(142, 20);
+            this.txtMaBP.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(346, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Mã BP";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(25, 376);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 13;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(130, 376);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(253, 376);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 411);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtMaBP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMaCV);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLuong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTenNV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHoNV);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvNV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvNV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.TextBox txtHoNV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenNV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLuong;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMaCV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaBP;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ DemoLINQToEF/Form1.cs
@@ -0,0 +1,78 @@
+﻿using DemoLINQToEF.BLL;
+using DemoLINQToEF.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoLINQToEF
+{
+    public partial class Form1 : Form
+    {
+        // kbao object from BLL
+        NhanVienService nv = new NhanVienService();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvNV.DataSource = nv.getAllNhanVien();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            NhanVienModel nvObj = new NhanVienModel();
+            nvObj.HoNV = txtHoNV.Text;
+            nvObj.TenNV = txtTenNV.Text;
+            nvObj.Luong = float.Parse(txtLuong.Text);
+            nvObj.MaCV = txtMaCV.Text;
+            nvObj.MaBP = txtMaBP.Text;
+
+            nv.InsertNV(nvObj);
+            dgvNV.DataSource = nv.getAllNhanVien(); // reload NV
+            MessageBox.Show("Insert Success!");
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNV.Text))
+            {
+                MessageBox.Show("Enter MaNV!");
+                return;
+            }
+            NhanVienModel nvObj = new NhanVienModel();
+
+            nvObj.MaNV = txtMaNV.Text;
+            nvObj.HoNV = txtHoNV.Text;
+            nvObj.TenNV = txtTenNV.Text;
+            nvObj.Luong = float.Parse(txtLuong.Text);
+            nvObj.MaCV = txtMaCV.Text;
+            nvObj.MaBP = txtMaBP.Text;
+
+            nv.UpdateNV(nvObj);
+            dgvNV.DataSource = nv.getAllNhanVien(); // reload NV
+            MessageBox.Show("Update Success!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNV.Text))
+            {
+                MessageBox.Show("Enter MaNV!");
+                return;
+            }
+
+            nv.DeleteNV(txtMaNV.Text);
+            dgvNV.DataSource = nv.getAllNhanVien(); // reload NV
+            MessageBox.Show("Delete Success!");
+        }
+    }
+}
